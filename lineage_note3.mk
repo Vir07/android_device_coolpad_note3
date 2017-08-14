e1f325963061e60e697f0e8f7f286df913601b3b
DX <virajv2k@gmail.com>
2017-08-14 23:56:20 +0530

    Update lineage_note3.mk

--- lineage_note3.mk
+++ lineage_note3.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Common stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/coolpad/note3/device.mk)
